a0ddb2516a9edf63743340262f1d73e84e556a1c
Philip S Doctor <philip.s.doctor@gmail.com>
2013-05-07 20:59:44 -0500

    finished #5 I'm moderately happy with it, could be happier

--- problem5.py
+++ problem5.py
@@ -8,8 +8,29 @@
 # good thing I already wrote a function to give me prime factors!
 
 import problem3
+from itertools import groupby
 
-for x in xrange(2, 21):
-    print problem3.get_prime_factors(x)
+def find_smallest_common_multiple_of(number):
+    prime_and_frequency = {}
+    for x in xrange(2, number + 1):
+        list_of_primes = problem3.get_prime_factors(x)
 
-print problem3.get_prime_factors(8)
+        # itertools lets us groupby on our list since it's already ordered
+        for prime, group in groupby(list_of_primes):
+            # okay so now we have each prime and how many times it's in our list, let's check our accumulated count
+            # if we already have at least that many of the prime factor, ignore it, otherwise update our count
+            frequency = len(list(group))
+            if prime not in prime_and_frequency:
+                prime_and_frequency[prime] = frequency
+            elif prime_and_frequency[prime] > frequency:
+                prime_and_frequency[prime] = frequency
+
+    # now we have a dict of each prime and their frequency, we can iterate over it to reach out output
+    smallest_number = 1
+    for prime, frequency in prime_and_frequency.iteritems():
+        smallest_number *= prime ** frequency
+
+    return smallest_number
+
+print find_smallest_common_multiple_of(20)
+
